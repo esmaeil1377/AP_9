cdfa868ca99f95b469f96432eaed062b8f6c33a1
pwer12515 <m.p.fattahpour@gmail.com>
2018-10-25 12:59:00 +0330

    ""

--- src/Model/Army.java
+++ src/Model/Army.java
@@ -1,4 +1,28 @@
 package Model;
 
 public class Army {
+   private int Level;
+   private int Id;
+
+    public void setLevel(int level) {
+        Level = level;
+    }
+
+    public int getLevel() {
+        return Level;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public int getId() {
+        return Id;
+    }
+    public void Attak(int BlockId){
+
+    }
+    public void Loot(int BlockId){
+
+    }
 }
