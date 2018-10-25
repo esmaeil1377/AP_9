812846300f9d9144f59f9cd1ded66260abd77645
pwer12515 <m.p.fattahpour@gmail.com>
2018-10-25 13:04:07 +0330

    Block text

--- src/Model/Block.java
+++ src/Model/Block.java
@@ -1,4 +1,43 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Block {
+    private int Level;
+    private int Id;
+    private ArrayList<Bazar> BazarArrayList;
+    private ArrayList<Padafand> PadafandArrayList;
+
+    public void setBazarArrayList(ArrayList<Bazar> bazarArrayList) {
+        BazarArrayList = bazarArrayList;
+    }
+
+    public ArrayList<Bazar> getBazarArrayList() {
+        return BazarArrayList;
+    }
+
+    public void setPadafandArrayList(ArrayList<Padafand> padafandArrayList) {
+        PadafandArrayList = padafandArrayList;
+    }
+
+    public ArrayList<Padafand> getPadafandArrayList() {
+        return PadafandArrayList;
+    }
+
+    public void setLevel(int level) {
+        Level = level;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public int getLevel() {
+        return Level;
+    }
+
 }
--- src/Model/Gilgdoni.java
+++ src/Model/Gilgdoni.java
@@ -3,5 +3,12 @@ package Model;
 import java.util.ArrayList;
 
 public class Gilgdoni {
-    private ArrayList<House> ArrayListhouses;
+    private ArrayList<House> HouseArrayList;
+    public int GetScore(){
+        int score = 0;
+        return score;
+
+
+    }
+
 }
