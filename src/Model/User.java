b12b87916d9db12c40e41db217958b909be4330b
Utop <pahang1378@gmail.com>
2018-10-25 12:40:31 +0330

    class user

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,4 +1,22 @@
 package Model;
 
 public class User {
+    int Score;
+    int Gills;
+
+    public int getScore() {
+        return Score;
+    }
+
+    public void setScore(int score) {
+        Score = score;
+    }
+
+    public int getGills() {
+        return Gills;
+    }
+
+    public void setGills(int gills) {
+        Gills = gills;
+    }
 }
