e12249d5ecc34e30f79dfb23bd065c2bd1ed3c1f
pwer12515 <m.p.fattahpour@gmail.com>
2018-10-25 12:22:30 +0330

    ""

--- src/Model/Add.java
+++ src/Model/Add.java
@@ -1,4 +1,40 @@
 package Model;
 
 public class Add extends Requests{
+    String item;
+    int NumberFloor;
+    int NumberUnit;
+    int BlockId;
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setNumberFloor(int numberFloor) {
+        NumberFloor = numberFloor;
+    }
+
+    public int getNumberFloor() {
+        return NumberFloor;
+    }
+
+    public void setBlockId(int blockId) {
+        BlockId = blockId;
+    }
+
+    public int getBlockId() {
+        return BlockId;
+    }
+
+    public void setNumberUnit(int numberUnit) {
+        NumberUnit = numberUnit;
+    }
+
+    public int getNumberUnit() {
+        return NumberUnit;
+    }
 }
