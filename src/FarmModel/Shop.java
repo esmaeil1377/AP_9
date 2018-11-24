5b5825867702157dbd7a835f6ea4fc204cb6b0d2
Utop <pahang1378@gmail.com>
2018-11-24 19:35:50 +0330

    1

--- src/FarmModel/Shop.java
+++ src/FarmModel/Shop.java
@@ -1,4 +1,5 @@
 package FarmModel;
 
 public class Shop {
+
 }
--- src/FarmModel/User.java
+++ src/FarmModel/User.java
@@ -1,4 +1,19 @@
 package FarmModel;
 
 public class User {
+    private int money;
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void AddMoney(int amountIncreaseMoney) {
+        int nowMoney = getMoney();
+        setMoney(nowMoney + amountIncreaseMoney);
+    }
+
 }
