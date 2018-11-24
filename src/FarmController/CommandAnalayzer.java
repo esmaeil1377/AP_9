2ec725b50b884153dbc0b2360467dd841a78fb80
lenovo <amir.mn.mosio@gamil.com>
2018-11-24 20:27:36 +0330

    a lot of things

--- src/FarmController/CommandAnalayzer.java
+++ src/FarmController/CommandAnalayzer.java
@@ -5,4 +5,9 @@ public class CommandAnalayzer {
     private String PickUp=" pickup [0-9] [0-9]";
     private String Cage="cage [0-9][0-9]";
     private String Plant="plant [0-9][0-9]";
+    private String Well="well";
+    private String StartWorkShop="start .*";
+    private String Upgrade="upgrade .*";
+    private String LoadCustom="load custom .*";
+    private String RunMap="run .*";
 }
