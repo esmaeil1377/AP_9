a7e7d3bac660b827966f265604b2a7ae03866dec
lenovo <amir.mn.mosio@gamil.com>
2018-11-24 20:34:09 +0330

    RequestAnalyzer

--- src/FarmController/RequestAnalayzer.java
+++ src/FarmController/RequestAnalayzer.java
@@ -1,6 +1,6 @@
 package FarmController;
 
-public class CommandAnalayzer {
+public class RequestAnalayzer {
     private String BuyAnimals="buy (cat|dog)";
     private String PickUp=" pickup [0-9] [0-9]";
     private String Cage="cage [0-9][0-9]";
@@ -10,4 +10,11 @@ public class CommandAnalayzer {
     private String Upgrade="upgrade .*";
     private String LoadCustom="load custom .*";
     private String RunMap="run .*";
+    private String SaveGame="save game .*";
+    private String LoadGame="load game .*";
+    private String Print="print .*";
+    private String Turn="turn [0-9]*";
+
+    public void RequestAnalayzer(String requestString){
+    }
 }
