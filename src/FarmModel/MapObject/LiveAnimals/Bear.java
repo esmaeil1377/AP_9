cd733d209dbf60a0ac0066b84586e1ab9490616e
lenovo <amir.mn.mosio@gamil.com>
2018-11-24 21:00:27 +0330

    little things

--- src/FarmModel/MapObject/LiveAnimals/Bear.java
+++ src/FarmModel/MapObject/LiveAnimals/Bear.java
@@ -1,5 +1,17 @@
 package FarmModel.MapObject.LiveAnimals;
 
 public class Bear extends WildAnimals {
+    private int PriceForSell;
+    private int PriceToBuy;
+
+    @Override
+    public int getPriceToBuy() {
+        return PriceToBuy;
+    }
+
+    @Override
+    public int getPriceForSell() {
+        return PriceForSell;
+    }
 
 }
--- src/FarmModel/MapObject/LiveAnimals/Lion.java
+++ src/FarmModel/MapObject/LiveAnimals/Lion.java
@@ -1,4 +1,17 @@
 package FarmModel.MapObject.LiveAnimals;
 
 public class Lion extends WildAnimals{
+    private int PriceForSell;
+    private int PriceToBuy;
+
+    @Override
+    public int getPriceToBuy() {
+        return PriceToBuy;
+    }
+
+    @Override
+    public int getPriceForSell() {
+        return PriceForSell;
+    }
+
 }
