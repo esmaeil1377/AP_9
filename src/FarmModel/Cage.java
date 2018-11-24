34b86e2ac292356cc67de5afafd972f53e4d9751
pwer12515 <m.p.fattahpour@gmail.com>
2018-11-24 20:09:49 +0330

    Block text

--- src/FarmModel/Cage.java
+++ src/FarmModel/Cage.java
@@ -3,6 +3,8 @@ package FarmModel;
 import FarmModel.MapObject.LiveAnimals.WildAnimals;
 
 public class Cage {
+    private int level;
+    private double time;
 
     public Cage(WildAnimals wildAnimals){
 
