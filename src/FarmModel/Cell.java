f48e6c754c211c7d5600ce9f9d71143845a43ab8
lenovo <amir.mn.mosio@gamil.com>
2018-11-24 19:46:59 +0330

    add map some method

---
+++ src/FarmModel/Cell.java
@@ -0,0 +1,15 @@
+package FarmModel;
+
+import FarmModel.MapObject.MapObject;
+
+import java.util.ArrayList;
+
+public class Cell {
+    private int XPosition;
+    private int YPsition;
+    ArrayList<MapObject> cellMapObject=new ArrayList<>();
+
+
+    public void AddCellAMapObject(MapObject mapObject){}
+    public void RremoveCellAMapObject(MapObject mapObject){}
+}
--- src/FarmModel/OutMapObject/Well.java
+++ src/FarmModel/OutMapObject/Well.java
@@ -1,11 +1,13 @@
 package FarmModel.OutMapObject;
 
 public class Well extends OutMapObject {
-    private int Level=0;
-    private int WaterOfTheBucket=0;
+    private int Level = 0;
+    private int WaterOfTheBucket = 0;
 
-    public void UpgradeWell(){}
+    public void UpgradeWell() {
+    }
 
-    public void FillTheBucket(){}
+    public void FillTheBucket() {
+    }
 
 }
