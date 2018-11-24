a291132dec48111321fa1a248488392f13e86733
lenovo <amir.mn.mosio@gamil.com>
2018-11-24 20:21:37 +0330

    little things

--- src/FarmController/CommandAnalayzer.java
+++ src/FarmController/CommandAnalayzer.java
@@ -2,4 +2,7 @@ package FarmController;
 
 public class CommandAnalayzer {
     private String BuyAnimals="buy (cat|dog)";
+    private String PickUp=" pickup [0-9] [0-9]";
+    private String Cage="cage [0-9][0-9]";
+    private String Plant="plant [0-9][0-9]";
 }
