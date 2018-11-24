176c114f6475f16eab895a7cd399a23b569cd0df
lenovo <amir.mn.mosio@gamil.com>
2018-11-24 19:36:07 +0330

    add some method

---
+++ src/FarmModel/OutMapObject/Well.java
@@ -0,0 +1,11 @@
+package FarmModel.OutMapObject;
+
+public class Well extends OutMapObject {
+    private int Level=0;
+    private int WaterOfTheBucket=0;
+
+    public void UpgradeWell(){}
+
+    public void FillTheBucket(){}
+
+}
