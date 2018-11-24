e97dfc458af8e1d1ecb81ba34590a57e9909e7a5
Utop <pahang1378@gmail.com>
2018-11-24 21:11:52 +0330

    1

--- src/FarmModel/Request/Clear.java
+++ src/FarmModel/Request/Clear.java
@@ -1,4 +1,17 @@
 package FarmModel.Request;
 
 public class Clear extends Request {
+    private String vehicleName;
+
+    public Clear(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
 }
--- src/FarmModel/Request/GoVehicle.java
+++ src/FarmModel/Request/GoVehicle.java
@@ -1,4 +1,17 @@
 package FarmModel.Request;
 
 public class GoVehicle extends Request {
+    private String vehicleName;
+
+    public GoVehicle(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
 }
