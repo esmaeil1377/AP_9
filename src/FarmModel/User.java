076010c96a1ee140d7075c11f454ca3924d1b72f
lenovo <amir.mn.mosio@gamil.com>
2018-11-25 14:00:03 +0330

    completing the uml

--- src/FarmModel/User.java
+++ src/FarmModel/User.java
@@ -1,10 +1,23 @@
 package FarmModel;
 
+import java.util.ArrayList;
+
 public class User {
     private int money;
+    ArrayList<Mission> Missions=new ArrayList<>();
+    private GameShop gameShop=new GameShop();
+
 
     public void setMoney(int money) {
         this.money = money;
+    }
+
+    public ArrayList<Mission> getMissions() {
+        return Missions;
+    }
+
+    public void setGameShop(GameShop gameShop) {
+        this.gameShop = gameShop;
     }
 
     public int getMoney() {
@@ -15,5 +28,6 @@ public class User {
         int nowMoney = getMoney();
         setMoney(nowMoney + amountIncreaseMoney);
     }
+    public void AddMissionToMissionsForUser(Mission mission){}
 
 }
