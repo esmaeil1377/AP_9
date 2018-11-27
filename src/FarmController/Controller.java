1645d79d6f251616c2dbd8941d7092fbe4d50781
lenovo <amir.mn.mosio@gamil.com>
2018-11-27 18:50:08 +0330

    add controller

---
+++ src/FarmController/Controller.java
@@ -0,0 +1,10 @@
+package FarmController;
+
+import FarmModel.ObjectInMap15_15.ObjectInMap15_15;
+import FarmModel.ObjectOutOfMap15_15ButInTheBorderOfPlayGround.ObjectOutOfMap15_15ButInTheBorderOfPlayGround;
+
+public class Controller {
+    public void AddItemToMap(ObjectInMap15_15 objectInMap15_15){}
+
+    public void AddItemToOutOfMap(ObjectOutOfMap15_15ButInTheBorderOfPlayGround objectOutOfMap15_15ButInTheBorderOfPlayGround){}
+}
