f7a1d4b637aef65be2cf451fcdfcfbd099ed71fd
lenovo <amir.mn.mosio@gamil.com>
2018-12-06 17:00:30 +0330

    craeting ObjectNotFoundInWareHouse class Exception

--- src/FarmModel/Farm.java
+++ src/FarmModel/Farm.java
@@ -1,5 +1,6 @@
 package FarmModel;
 
+import FarmController.Exceptions.ObjectNotFoundInWareHouse;
 import FarmModel.ObjectInMap15_15.Grass;
 import FarmModel.ObjectInMap15_15.LiveAnimals.Animals;
 import FarmModel.ObjectInMap15_15.ObjectInMap15_15;
@@ -205,10 +206,16 @@ public class Farm {
         return null;
     }
 
-    public void getObjectInWareHouse(Object object){
+
+    //i don't think that it  could work but i'll look after.
+    public Object getObjectInWareHouse(Object object){
         for(Object objectinWareHouse:wareHouse.getWareHouseList()){
-            if(object)
+           if(objectinWareHouse.toString().equals(object.toString())){
+               return objectinWareHouse;
+            }
         }
+        throw new ObjectNotFoundInWareHouse();
+
     }
 
 }
